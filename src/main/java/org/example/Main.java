dcebfb95bcf9e1e545bfa44d0002a8462ac6a8a9
sergey <goncharovjava@gmail.com>
2024-05-15 20:39:51 +0700

    Create Solution class

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+
+package org.example;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(3781);
+    }
+    public static String intToRoman(int num){
+        //метод принимает на вход арабские цифры, возвращает римские
+
+
+
+
+
+
+        return null;
+    }
+}
