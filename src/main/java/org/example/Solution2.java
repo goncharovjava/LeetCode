ec65314215fbe6f5143ea1b6fbc80ff585f18f26
sergey <goncharovjava@gmail.com>
2024-05-16 20:42:40 +0700

    add HashMap map

--- src/main/java/org/example/Solution2.java
+++ src/main/java/org/example/Solution2.java
@@ -1,12 +1,23 @@
 package org.example;
 
+import java.util.HashMap;
+
 public class Solution2 {
     static public void main(String[] args){
-        System.out.println();
+        System.out.println(romanToInt("LVIII"));
     }
 
-    static public int RomanToInteger(String s){
+    static public int romanToInt(String s){
         //Метод принимает на вход римское число, возвращает арабское
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("I",1);
+        map.put("V",5);
+        map.put("X",10);
+        map.put("L",50);
+        map.put("C",100);
+        map.put("D",500);
+        map.put("M",1000);
+
 
 
 
