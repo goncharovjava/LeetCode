d27aba20af653c42a3171ff37184c5e44af3058f
sergey <goncharovjava@gmail.com>
2024-05-16 20:04:20 +0700

    Create class Solution2

---
+++ src/main/java/org/example/Solution2.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Solution2 {
+    static public void main(String[] args){
+        System.out.println();
+    }
+
+    static public int RomanToInteger(String s){
+        //Метод принимает на вход римское число, возвращает арабское
+
+
+
+
+
+        return 0;
+    }
+}
