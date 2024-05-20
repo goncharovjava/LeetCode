2d5e6bde22aa774bc37600fd72d937803e845f00
sergey <goncharovjava@gmail.com>
2024-05-20 21:25:22 +0700

    solution

---
+++ src/main/java/org/example/Solution3.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class Solution3 {
+    public static void main(String[] args){
+        String[] s = {"flower","flower","flower","flower"};
+        System.out.println(longestCommonPrefix(s));
+    }
+    public static String longestCommonPrefix(String[] strs) {
+        //Проверка на пустооту массива
+        if(strs==null || strs.length==0){
+            return "";
+        }
+
+        //Выносим первое значение массива по которому будем искать
+        String prefix=strs[0];
+        //Проходим по массиву начиная со втророго значения
+        for(int i=1;i<strs.length;i++){
+            //пока не найдется совпадение уменьшаем длину строки prefix
+            while(strs[i].indexOf(prefix)!=0){
+                prefix=prefix.substring(0,prefix.length()-1);
+                //Если уменьшили длину строки prefix до нуля
+                if(prefix.isEmpty()){
+                    return "";
+                }
+            }
+        }
+        return prefix;
+    }
+}
