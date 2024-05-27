3cc29cbd3a8ec78538d23030c0cddc78c4d434dc
sergey <goncharovjava@gmail.com>
2024-05-27 20:11:01 +0700

    Add commit

--- src/main/java/org/example/Solution26.java
+++ src/main/java/org/example/Solution26.java
@@ -37,7 +37,7 @@ public class Solution26 {
         //Если длиннна массива ровна 1 то вернуть результат
         if (nums.length == 1){
             return result;
-        //иначе переписать массив num
+        //иначе переписать массив num зная количество уникальных символов count
         }else {
             for (int i = 1; i < current.length; i++) {
                 if (i < count){
