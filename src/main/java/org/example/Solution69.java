8ec2cd817cf0fe4d384b1b01f2ec8e8459f223e0
sergey <goncharovjava@gmail.com>
2024-05-30 22:08:06 +0700

    add Solution69

---
+++ src/main/java/org/example/Solution69.java
@@ -0,0 +1,14 @@
+package org.example;
+
+
+public class Solution69 {
+    public static void main(String[] arg) {
+
+        System.out.println(addBinary(4));
+    }
+
+    public static int addBinary(int x) {
+       return (int) Math.abs(Math.sqrt(x));
+
+    }
+}
